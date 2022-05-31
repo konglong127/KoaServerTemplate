adbf895b1226157f47565190840e19adec4d3c42
konglong127 <1628253184@qq.com>
2022-05-31 21:46:56 +0800

    main.js change

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ const path = require('path');
 const config = require(path.resolve(__dirname, './config/config')).server;
 const http = require(path.resolve(__dirname, './http/http'));
 
-if (config.type == 'http') {
-  http.create(config.workers[0] || { port: 80, role: 'worker' }, false);
-} else if (config.type == 'http2') {
-  
-} else {
+if (config.type == 'https') {
   http.create(config.workers[0] || { port: 443, role: 'worker' }, false);
+}else {
+  http.create(config.workers[0] || { port: 80, role: 'worker' }, false);
 }
